a8eb487cab6d1c5ad2baef603b91d6af315f2b2d
Ariel <parnell.ariel@yahoo.com>
2023-08-14 05:14:41 -0500

    imported usestate from react and satdata from json file; also set state var sat

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
 import Banner from "./components/Banner";
-
+import { useState } from "react";
+import satData from "./components/satData";
 
 function App() {
+  const [sat, setSat] = useState(satData);
   return (
     <div>
       <Banner />
